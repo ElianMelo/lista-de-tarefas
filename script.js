afcd6a3ad95f6a67403663925a2cf38fcb38b996
ElianMelo <elianmelomorais@gmail.com>
2018-11-16 17:35:50 -0200

    Estrutura Avançada

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var ids = 0;
 function adicionaTarefa(event) {
     // Verifica se a tecla apertada pelo usuário foi enter
     if(event.which == 13 || event.keyCode == 13){
-        // Pega o valor da tarefa no campo gerador de tarefas e troca o valor do id
+        // Pega o valor da tarefa no campo gerador de tarefas e aumenta o valor do id
         var texto = document.getElementById("geradorTarefa").value;
         ids += 1;
 
@@ -15,15 +15,15 @@ function adicionaTarefa(event) {
         // Limpa o campo do gerador de tarefas
         document.getElementById("geradorTarefa").value = "";
 
-        // Cria o div que será enviado atribuindo id e estilo
+        // Cria a div que será enviado atribuindo id e estilo
         var divPrincipal = document.createElement("div");
         divPrincipal.className = "estiloDiv";
-        divPrincipal.id = ids;
+        divPrincipal.id = ids + "D";
 
         // Monta a estrutura que será enviada
         divPrincipal.innerHTML =
         "<div class='botaoCheck' onclick='return chequeTarefa(event);' id=" + ids + ">\u2713</div>" +
-        "<label class='estiloTexto' id=" + ids + ">" + texto + "</label>" +
+        "<label class='estiloTexto' id=" + ids + "T" + ">" + texto + "</label>" +
         "<div class='botaoExcluir' onclick='return excluiTarefa(event);' id=" + ids + ">\u00D7</div>";
 
         // Envia a estrutura
@@ -31,16 +31,36 @@ function adicionaTarefa(event) {
 
         return false;
         }
-    }else{
+    } else {
         return true;
     }
 }
 
 function excluiTarefa(event) {
-    excluir = document.getElementById(event.srcElement.id);
+    // Exclui o elemento clickado
+
+    var excluir = document.getElementById(event.srcElement.id + "D");
     excluir.parentNode.removeChild(excluir);
 }
 
 function chequeTarefa(event) {
+    // Marca ou desmarcar uma tarefa
 
+    // Pega o texto o botão e a div que são editados
+    var alterarTexto = document.getElementById(event.srcElement.id + "T");
+    var alterarBotao = document.getElementById(event.srcElement.id);
+    var alterarDiv = document.getElementById(event.srcElement.id + "D");
+
+    // Altera o texto o botão e a div
+    if (alterarTexto.className != "estiloTextoAlterado"){
+        alterarTexto.className = "estiloTextoAlterado";
+        alterarBotao.className = "botaoCheckAtivado";
+        alterarDiv.className = "estiloDivAlterado";
+
+    // Retorna o texto o botão e a div ao seu estado normal
+    } else if (alterarTexto.className == "estiloTextoAlterado"){
+        alterarTexto.className = "estiloTexto";
+        alterarBotao.className = "botaoCheck";
+        alterarDiv.className = "estiloDiv";
+    }
 }
