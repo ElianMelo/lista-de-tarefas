704e0bfb53ad3230d9d57161ae732a0e8d594b1f
ElianMelo <elianmelomorais@gmail.com>
2018-11-16 14:51:21 -0200

    Cores e Exclusão

--- script.js
+++ script.js
@@ -10,29 +10,28 @@ function adicionaTarefa(event) {
         // Limpa o campo do gerador de tarefas
         document.getElementById("geradorTarefa").value = "";
 
-        // Cria um input e associa o tipo checkbox a ele
-        var tarefa = document.createElement("input");
-        tarefa.type = "checkbox";
-        tarefa.id = ids;
-        tarefa.className = "estiloCaixa";
+        // Cria um <label> que leva o nome da tarefa
+        var nomeTarefa = document.createElement("label");
+        nomeTarefa.id = ids;
+        nomeTarefa.className = "estiloTexto";
+        nomeTarefa.appendChild(texto);
 
-        // Cria um label que dá um nome ao checkbox
-        var nome = document.createElement("label");
-        nome.htmlFor = ids;
-        nome.className = "estiloTexto";
-        nome.appendChild(texto);
-
-        //Cria uma div
-        var div = document.createElement("div");
-        div.id = ids;
-        div.className = "estiloDiv";
+        // Cria a div principal
+        var divPrincipal = document.createElement("div");
+        divPrincipal.innerHTML = "<span onclick='return excluiTarefa(event);' id=" + ids + "> X </span>";
+        divPrincipal.id = ids;
+        divPrincipal.className = "estiloDiv";
 
         // Envia para o corpo do programa uma div com uma tarefa e o seu nome
-        document.body.appendChild(div);
-        document.getElementById(ids).appendChild(tarefa);
-        document.getElementById(ids).appendChild(nome);
+        document.body.appendChild(divPrincipal);
+        document.getElementById(ids).appendChild(nomeTarefa);
         return false;
     }else{
         return true;
     }
+}
+
+function excluiTarefa(event) {
+    excluir = document.getElementById(event.srcElement.id);
+    excluir.parentNode.removeChild(excluir);
 }
